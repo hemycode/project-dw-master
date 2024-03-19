7c86a81fc22971e67856e847b3a584a9b7b734e0
Hemily <hemilyarmazan@outlook.com>
2024-03-19 16:11:43 -0300

    first commit

---
+++ script/cadastro.js
@@ -0,0 +1,54 @@
+const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
+
+const formCadastro = document.querySelector("#cadastro-form")
+formCadastro.addEventListener("submit", cadastroUsuario)
+
+async function cadastroUsuario(e){   
+    e.preventDefault()
+    const formData = Object.fromEntries(new FormData(e.target))
+    console.log(formData)
+
+    formData.avatar.type = "png"
+
+    let resposta = await fetch(url,{
+        method:"POST",
+        body:JSON.stringify(
+            {
+                "name": formData.name,
+                "email": formData.email,
+                "user_type_id": Number(formData.user_type_id),
+                "password": formData.password,
+                "cpf_cnpj": formData.cpf_cnpj,
+                "terms": formData.termos === "sim",
+                "birthday": formData.birthday,
+                "avatar": formData.avatar,
+                "attachment": formData.attachment
+            }
+        ),
+        headers:{
+            'Content-Type': 'application/json'
+        }        
+    });
+
+    let response = await resposta.json();
+
+    console.log(response)
+    
+    
+    if (response.data.statusCode != 200) {
+        if (typeof response.data.errors === "object") {
+            const erros = Object.entries(response.data.errors);
+            erros.forEach(([_, erro]) => {
+                alert(erro)
+            });
+            return;
+        };
+
+        if (!response.data.erros) return;
+
+        alert(response.data.errors)
+    }
+
+    alert("Cadastro feito com sucesso");
+    window.location.href = "login.html";
+}
